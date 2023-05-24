7466b580930f453237dd526b9a7715cbf1385f21
Jain_Sarthak_97162 <118130490+Sarthakj-github@users.noreply.github.com>
2023-05-25 00:43:28 +0530

    Update Print numbers from n-1.py

--- Print numbers from n-1.py
+++ Print numbers from n-1.py
@@ -1,11 +1,11 @@
 def print1_n(n):
 	print(n,end=' ')
-  if n!=1:
+	if n!=1:
 		print1_n(n-1)
 
 n=int(input("Enter a number:"))
 if n<1:
     print("Invalid Input!")
 else:
-    print(f"Numbers from 1 to {n} are:-")
+    print(f"Numbers from {n} to 1 are:-")
     print1_n(n)
