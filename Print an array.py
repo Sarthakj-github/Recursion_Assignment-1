56def87abf1bbddf5f5b6dac1df0c3caf9031c56
Jain_Sarthak_97162 <118130490+Sarthakj-github@users.noreply.github.com>
2023-05-25 00:52:26 +0530

    Update Print an array.py

--- Print an array.py
+++ Print an array.py
@@ -1,10 +1,10 @@
 def print_arr(arr,i,n):
-	if i<n:
-		print(arr[i],end=' ')
-		print_arr(arr,i+1,n)
+    if i<n:
+        print(arr[i],end=' ')
+        print_arr(arr,i+1,n)
 
 arr=eval(input("Enter an array:"))
 if arr==[]:
-	print("Empty Array!")
+    print("Empty Array!")
 else:
-	print_arr(arr,0,len(arr))
+    print_arr(arr,0,len(arr))
