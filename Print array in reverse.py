9dca880def05be63c01559abd4aabf36c9ba5a07
Jain_Sarthak_97162 <118130490+Sarthakj-github@users.noreply.github.com>
2023-05-25 00:57:15 +0530

    Update Print array in reverse.py

--- Print array in reverse.py
+++ Print array in reverse.py
@@ -1,10 +1,11 @@
 def print_arr(arr,i):
     if i:
 		print(arr[i-1])
-        print_arr(arr,i-1)
+		print_arr(arr,i-1)
 
 arr=eval(input("Enter an array:"))
 if arr==[]:
     print("Empty Array!")
 else:
+	print("Printing array in reverse order:-")
     print_arr(arr,len(arr))
